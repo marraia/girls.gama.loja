cd04ce158e51d5e769a67433b36aaa4f0c2533ae
Fernando Mendes <foes@gft.com>
2021-02-02 20:13:38 -0300

    Loja

---
+++ Girls.Gama2/Entidades/Boleto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Girls.Gama2.Entidades
+{
+    public class Boleto
+    {
+        private const int DiasVencimento = 15;
+        private const decimal Juros = 0.10M;
+
+        public Boleto(string cpf,
+                        decimal valor,
+                        string descricao)
+        {
+            Cpf = cpf;
+            Valor = valor;
+            Descricao = descricao;
+
+            DataEmissao = DateTime.Now;
+            Confirmacao = false;
+        }
+
+        public Guid CodigoBarra { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataEmissao { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public bool Confirmacao { get; set; }
+        public string Cpf { get; set; }
+        public string Descricao { get; set; }
+
+        public void GerarBoleto()
+        {
+            CodigoBarra = Guid.NewGuid();
+            DataVencimento = DataEmissao.AddDays(DiasVencimento);
+        }
+
+        public void CalcularJuros()
+        {
+            var taxa = Valor * Juros;
+            Valor += taxa;
+        }
+
+        public void Pagar()
+        {
+            DataPagamento = DateTime.Now;
+            Confirmacao = true;
+        }
+    }
+}
---
+++ Girls.Gama2/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using Girls.Gama2.Entidades;
+using System;
+using System.Collections.Generic;
+
+namespace Girls.Gama2
+{
+    class Program
+    {
+        private static List<Boleto> listaBoletos;
+        static void Main(string[] args)
+        {
+            listaBoletos = new List<Boleto>();
+
+            while (true)
+            {
+                Console.WriteLine("================== Loja das meninas da Gama Academy ============================");
+                Console.WriteLine("Selecione uma opção");
+                Console.WriteLine("1-Compra | 2- Pagamento");
+
+                var opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Comprar();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void Comprar()
+        {
+            Console.WriteLine("Digite o valor da compra:");
+            var valor = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o CPF do cliente:");
+            var cpf = Console.ReadLine();
+
+            Console.WriteLine("Preeencha uma descrição caso necessário");
+            var descricao = Console.ReadLine();
+
+            var boleto = new Boleto(cpf, valor, descricao);
+            boleto.GerarBoleto();
+
+            Console.WriteLine($"Boleto gerado com sucesso com o número {boleto.CodigoBarra} com data de vencimento para o dia {boleto.DataVencimento} ");
+
+            listaBoletos.Add(boleto);
+        }
+    }
+}
+
